f40e8c80be7c5bd938a393b268f2268de21086c3
Kaan <64705623+reed098@users.noreply.github.com>
2022-04-10 22:51:52 +0300

    Add files via upload

---
+++ Deneme.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deneme : MonoBehaviour
+{
+
+    public Transform target;
+
+    float t = 0.2f;
+    
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        t = Mathf.PingPong(0.2f * Time.time,1f);
+
+        transform.rotation = Quaternion.Slerp(Quaternion.Euler(0,90,0), Quaternion.Euler(0,-90,0), t);
+
+
+
+    }
+
+
+}
