59304e521fc6e348f1f361e01fa2ad64aef506e0
Kaan <64705623+reed098@users.noreply.github.com>
2022-04-16 22:15:29 +0300

    Update and rename Kamera Takip Yumuşak Geçişli.cs to Kamera Takip Yumuşak Geçişli ve clamp.cs

--- Kamera Takip Yumuşak Geçişli ve clamp.cs
+++ Kamera Takip Yumuşak Geçişli ve clamp.cs
@@ -16,9 +16,11 @@ public class KameraTakip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //float xClamp = Mathf.Clamp(transform.position.x,0,0); //Kamerann nesneyi x yönünde takip etmemesini isterseniz burayı ve targetpos.x = xClamp kodunu açın.
         Vector3 targetpos = target.transform.position + mesafe; //Kameranın her zaman bu mesafeden takip etmesini söyledik.
+        //targetpos.x = xClamp; //Nesnemizi her zaman takip etmesi için yazdığımız kod olan targetpos.x e xClamp ı atıyoruz.
         transform.position = Vector3.Lerp(transform.position,targetpos,6f * Time.deltaTime);
-        //vector.lerp ile yumuşak geçiş sağladık ve target objesi durduğunda veya hizlandığında yumuşak olarak takip edecek.
+            //vector.lerp ile yumuşak geçiş sağladık ve target objesi durduğunda veya hizlandığında yumuşak olarak takip edecek.
 
         
     }
